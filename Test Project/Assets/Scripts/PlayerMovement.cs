f3cd6961cf5421ccf5cda3a38ea75312fdb1f192
Yankı Ufuk Dülger <yankiudulger@gmail.com>
2022-06-30 23:43:07 +0300

    characterMovement3

--- Test Project/Assets/Scripts/PlayerMovement.cs
+++ Test Project/Assets/Scripts/PlayerMovement.cs
@@ -2,22 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterControls : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    
     public CharacterController2D controller;
-
     public float runSpeed = 40f;
+
     float horizontalMove = 0f;
-    void update()
+    bool jump = false;
+    void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        
     }
+
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.deltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
-}
-    
 
+}
